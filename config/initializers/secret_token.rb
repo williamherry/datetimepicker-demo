8832c096bf4741618b08b771768448f5b8f314f0
William Herry <WilliamHerryChina@Gmail.com>
2013-07-21 23:03:54 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DatetimepickerDemo::Application.config.secret_key_base = '4f04adea961c8eccfe73809c77a918c6a6ff53484bd905332d5f869d497161df1a4d6ef8b958ab458ff31c6de84d46873555c4ab14d431f0eb9555ad611ece69'
